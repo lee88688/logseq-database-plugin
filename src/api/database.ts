c6b84bfcf3c6d191be81b9a7b57fccf2a68e34ea
lee88688@163.com <lee>
2023-06-22 00:48:45 +0800

    add database base api

---
+++ src/api/database.ts
@@ -0,0 +1,100 @@
+import {
+  COLUMN_PREFIX,
+  ColumnConfig,
+  decodeColumnName,
+  encodeColumnName,
+  PageAlreadyExistsError,
+  PageCreateError,
+  propertyColumnKey,
+  propertyKey
+} from './index'
+
+export class Database {
+  pageName: string
+  propertyBlockId = ''
+  columns: Record<string, ColumnConfig> = {}
+  hasMetaCache = false
+
+  constructor(pageName = '') {
+    this.pageName = pageName
+  }
+
+  getInitialPageProperties() {
+    return {
+      [propertyKey('version')]: 1
+    }
+  }
+
+  async fetchDatabaseMeta(force = false) {
+    if (this.hasMetaCache && !force) return
+
+    if (!this.pageName) {
+      throw new Error('page name is not set!')
+    }
+
+    const blocks = await logseq.Editor.getPageBlocksTree(this.pageName)
+    const propertyBlock = blocks[0]
+    if (propertyBlock?.['preBlock?']) {
+      this.propertyBlockId = propertyBlock.uuid
+
+      const properties = propertyBlock.properties
+      const columns: Record<string, ColumnConfig> = {}
+      properties &&
+        Object.keys(propertyBlock.properties ?? {})
+          .filter((key) => key.startsWith(COLUMN_PREFIX))
+          .forEach((key) => {
+            const columnName = decodeColumnName(key.slice(COLUMN_PREFIX.length))
+            const json = properties[columnName]
+            columns[columnName] = JSON.parse(json)
+          })
+      this.columns = columns
+
+      this.hasMetaCache = true
+      return
+    }
+
+    throw new Error('page does not have property block.')
+  }
+
+  async createDatabase(pageName: string) {
+    const hasPageName = await logseq.Editor.getPage(pageName)
+    if (hasPageName) {
+      throw new PageAlreadyExistsError(`${pageName} already exists`)
+    }
+
+    const page = await logseq.Editor.createPage(pageName, this.getInitialPageProperties(), { redirect: false })
+    if (!page) {
+      throw new PageCreateError('page create failed')
+    }
+
+    return page
+  }
+
+  async createColumn(name: string, config: ColumnConfig) {
+    await this.fetchDatabaseMeta()
+
+    const encodeName = encodeColumnName(name)
+
+    await logseq.Editor.upsertBlockProperty(this.propertyBlockId, propertyColumnKey(encodeName), JSON.stringify(config))
+  }
+
+  async removeColumn(name: string) {
+    throw new Error('not implemented!')
+  }
+
+  async renameColumn(name: string) {
+    throw new Error('not implemented!')
+  }
+
+  async createRow<T>(row: T): Promise<string> {
+    throw new Error('not implemented!')
+  }
+
+  async removeRow(id: string): Promise<boolean> {
+    throw new Error('not implemented!')
+  }
+
+  async moveRow(id: string, to: string): Promise<boolean> {
+    throw new Error('not implemented!')
+  }
+}
---
+++ src/api/index.ts
@@ -0,0 +1,34 @@
+export function encodeColumnName(name: string) {
+  return name.replaceAll(' ', '+')
+}
+
+export function decodeColumnName(name: string) {
+  return name.replaceAll('+', ' ')
+}
+
+export class PageAlreadyExistsError extends Error {}
+
+export class PageCreateError extends Error {}
+
+enum ColumnType {
+  Boolean = 'boolean',
+  Number = 'number',
+  String = 'string',
+  Select = 'select',
+  MultiSelect = 'multiSelect',
+  Tag = 'tag'
+}
+
+export type ColumnConfig =
+  | {
+      type: ColumnType.Boolean | ColumnType.Number | ColumnType.String
+    }
+  | {
+      type: ColumnType.Select | ColumnType.MultiSelect | ColumnType.Tag
+      enums: string[]
+    }
+
+const PROPERTY_PREFIX = 'database.'
+export const COLUMN_PREFIX = `${PROPERTY_PREFIX}.column`
+export const propertyKey = (...keys: string[]) => `${PROPERTY_PREFIX}${keys.join('.')}`
+export const propertyColumnKey = (name: string) => propertyKey('column', name)
